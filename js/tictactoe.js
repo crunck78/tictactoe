b578d5a77bf3f8d23139bfedac9b03e0f3ae755b
crunck78 <crunck78@googlemai.com>
2020-07-08 06:50:15 +0200

    refactoring Code

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,45 +1,64 @@
 alert('play tic tac toe'); //test
 
-var player = 0;
+var allPlayers = [	
+	{ 	'number' : 1,
+		'mark' : 'X',
+		'color' : 'bg-primary'
+	},
+	{
+		'number' : 2,
+		'mark' : 'O',
+		'color' : 'bg-danger'
+	}
+];
+
 var cells = [];
+var player = 0;
+var marks = 0;
+
+function initCells() {
+	for(var i = 0; i < 9; i++) {
+		cells.push( document.getElementById('cell' + i));
+	}
+}
 
 function startGame() {
-	player = 1;
-	document.getElementById('player-turn').innerHTML = 'Player ' +player+ ' select a cell.';
+	initCells();
+	document.getElementById('player-turn').innerHTML = 'Player ' +allPlayers[player]['number']+ ' select a cell.';
 }
 
 function mark(nr) {
-
-	//disable button
+	marks++;
+	//hidde button
 	document.getElementById('select' + nr).classList.add('d-none');
-	if( player == 1) {//player1 mark
-		document.getElementById('cell' + nr).classList.add('bg-primary');
-		document.getElementById('cell' + nr).innerHTML = 'X';
-		player = 2; // chnage player
-	} else {//player2 mark
-		document.getElementById('cell' + nr).classList.add('bg-danger');
-		document.getElementById('cell' + nr).innerHTML = 'O';
-		player = 1; //change player
+	//mark selection
+	cells[nr].classList.add(allPlayers[player]['color']);
+	cells[nr].innerHTML = allPlayers[player]['mark'];
+
+	checkBoard();
+	//change player
+	player = (player == allPlayers.length - 1) ? 0 : (player + 1);
+	document.getElementById('player-turn').innerHTML = 'Player ' +allPlayers[player]['number']+ ' select a cell.';
+}
+
+function checkBoard() {
+	var win = 0;
+	var lineOffset = 3; // board dimention 3x3
+	var cellOffset = 1; // row line
+	checkLines( lineOffset, cellOffset);
+}
+
+function checkLines( lineOffset, cellOffset) {
+	var nextCell = 1;
+	var lastCell = cells.length;
+	var match = 0;
+	for( var i = 0; i < 3; i++) { // 3 for board dimention 3x3
+		if( cells[i * lineOffset] != cells[nextCell * cellOffset] ) {
+			match = 0;
+			break; // no match found go to next line
+		}
+		match = 1;
 	}
-
-	chekcBoard();
-	document.getElementById('player-turn').innerHTML = 'Player ' +player+ ' select a cell.';
-}
-
-function chekcBoard() {
-	checkHorizontals();
-	checkVerticals();
-	checkDiagonals();
-}
-
-function checkHorizontals(){
-
-}
-
-function checkVerticals(){
-
-}
-
-function checkDiagonals(){
-
+	
+	return match;
 }
